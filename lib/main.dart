b9b02a7a7f7055ece83c654d123e18d42fdf6ccc
Ashok <ashok@N14545.local>
2023-07-03 11:26:52 +0530

    statefull component added

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,15 @@ void main() {
   ));
 }
 
-class TestIdCard extends StatelessWidget {
+class TestIdCard extends StatefulWidget {
   const TestIdCard({super.key});
 
+  @override
+  State<TestIdCard> createState() => _TestIdCardState();
+}
+
+class _TestIdCardState extends State<TestIdCard> {
+  int currentLevel = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,80 +25,82 @@ class TestIdCard extends StatelessWidget {
         backgroundColor:Colors.grey[850],
         elevation: 0,
       ),
-      body: IdCard(),
-    );
-  }
-}
-
-class IdCard extends StatelessWidget {
-  const IdCard({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return  Padding(
-      padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Center(
-            child: CircleAvatar(
-              backgroundImage: AssetImage('assets/Ashok_2nt.jpeg'),
-              radius: 40,
-            ),
-          ),
-          Divider(
-            height: 90,
-            color: Colors.grey[800],
-          ),
-          Text(
-            'NAME',
-            style: TextStyle(
-              color:Colors.grey,
-              letterSpacing: 2
-            )),
-          SizedBox(height: 10),
-          Text(
-              'Ashok',
-              style: TextStyle(
-                  color:Colors.amberAccent[200],
-                  letterSpacing: 2,
-                fontSize: 28,
-                fontWeight: FontWeight.bold
-              )),
-          SizedBox(height: 30),
-          Text(
-              'CURRENT LEVEL',
-              style: TextStyle(
-                  color:Colors.grey,
-                  letterSpacing: 2
-              )),
-          SizedBox(height: 10),
-          Text(
-              '10',
-              style: TextStyle(
-                  color:Colors.amberAccent[200],
-                  letterSpacing: 2,
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold
-              )),
-          SizedBox(height: 30),
-          Row(
-            children: [
-              Icon(Icons.email,color:Colors.grey[400]),
-              SizedBox(width: 10),
-              Text('aruashok17@gmail.com',
-              style: TextStyle(
-                color: Colors.grey[100],
-                fontSize: 18,
-                letterSpacing: 1
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){
+            setState(() {
+              currentLevel++;
+            });
+          },
+          child: Icon(Icons.update),
+          tooltip: 'Upgarde skill',
+        ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/Ashok_2nt.jpeg'),
+                radius: 40,
               ),
-              )
-            ],
-          )
-        ],
-      ),
+            ),
+            Divider(
+              height: 90,
+              color: Colors.grey[800],
+            ),
+            Text(
+                'NAME',
+                style: TextStyle(
+                    color:Colors.grey,
+                    letterSpacing: 2
+                )),
+            SizedBox(height: 10),
+            Text(
+                'Ashok',
+                style: TextStyle(
+                    color:Colors.amberAccent[200],
+                    letterSpacing: 2,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold
+                )),
+            SizedBox(height: 30),
+            Text(
+                'CURRENT LEVEL',
+                style: TextStyle(
+                    color:Colors.grey,
+                    letterSpacing: 2
+                )),
+            SizedBox(height: 10),
+            Text(
+                '$currentLevel',
+                style: TextStyle(
+                    color:Colors.amberAccent[200],
+                    letterSpacing: 2,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold
+                )),
+            SizedBox(height: 30),
+            Row(
+              children: [
+                Icon(Icons.email,color:Colors.grey[400]),
+                SizedBox(width: 10),
+                Text('aruashok17@gmail.com',
+                  style: TextStyle(
+                      color: Colors.grey[100],
+                      fontSize: 18,
+                      letterSpacing: 1
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      )
     );
   }
 }
+
+
 
 
